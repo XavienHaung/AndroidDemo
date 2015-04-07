31cb208ebcf9c3d37ebd69ec2255163d61675e50
XavienHaung <huang.xavien@outlook.com>
2015-04-07 14:39:53 +0800

    merge code

--- src/main/java/com/xavien/utils/XLog.java
+++ src/main/java/com/xavien/utils/XLog.java
@@ -2,21 +2,21 @@ package com.xavien.utils;
 
 import android.util.Log;
 
+import com.xavien.demo.BuildConfig;
+
 /**
  * Created by Xavien on 15-3-31.
  */
 public class XLog {
 
-    private static final boolean DEBUG = true;
-
     public static void i(String TAG, String msg){
-        if(DEBUG) {
+        if(BuildConfig.DEBUG) {
             Log.i(TAG, msg);
         }
     }
 
     public  static void e(String TAG, String msg){
-        if(DEBUG){
+        if(BuildConfig.DEBUG){
             Log.e(TAG, msg);
         }
     }
